dfef56598e959e54abf9ceb746364a8165d31444
mulfis <luthfiassidiq12@gmail.com>
2024-06-06 16:25:08 +0700

    css updated

--- src/views/home/LandingSection.vue
+++ src/views/home/LandingSection.vue
@@ -5,17 +5,17 @@ import { Sam } from '@/assets/img'
 </script>
 
 <template>
-  <section class="bg-white flex m-auto justify-around h-full pt-28 2xl:pt-[12rem]" name="landing" id="landing">
+  <section class="bg-white flex m-auto justify-around h-full pt-28 2xl:pt-[10rem]" name="landing" id="landing">
     <div class="m-auto mr-10 pr-10 w-1/2">
       <div class="p-2 flex">
         <Fire class="mr-2" fill="#14b8a6" width="25" height="25" />
-        <p class="text-lg font-medium text-teal-700">Super Trendies Tailwind Template</p>
+        <p class="text-lg 2xl:text-3xl font-medium text-teal-700">Super Trendies Tailwind Template</p>
       </div>
       <div class="p-2">
-        <h1 class="font-bold text-5xl text-teal-950">Text your website to the next level with <span class="text-teal-500">Samfly</span><span class="text-teal-600">UI</span></h1>
+        <h1 class="font-bold text-5xl 2xl:text-7xl text-teal-950">Text your website to the next level with <span class="text-teal-500">Samfly</span><span class="text-teal-600">UI</span></h1>
       </div>
       <div class="p-2 py-4">
-        <p class="text-gray-900">Save hundreds of hours trying to create and develop a dashboard from scratch. The fastest, most responsive & trendiest dashboard is here. Seriously.</p>
+        <p class="text-gray-900 2xl:text-2xl">Save hundreds of hours trying to create and develop a dashboard from scratch. The fastest, most responsive & trendiest dashboard is here. Seriously.</p>
       </div>
       <div class="flex p-2">
         <ButtonPrimary text="Get Started Now" type="solid"/>
@@ -23,7 +23,7 @@ import { Sam } from '@/assets/img'
       </div>
     </div>
     <div class="justify-end flex m-auto pl-10 w-1/2">
-      <img :src="Sam" alt="Sam" class="h-[440px] 2xl:h-[600px] rounded-xl">
+      <img :src="Sam" alt="Sam" class="h-[440px] 2xl:h-[660px] rounded-xl">
     </div>
   </section>
 </template>
